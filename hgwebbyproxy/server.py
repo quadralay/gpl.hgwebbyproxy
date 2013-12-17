02138ae4e3554d3beaf62f87d469f551a82ef216
Ben Allums <allums@webworks.com>
2013-12-17 10:33:23 -0600

    Switched over to waitress due to problems with Rocket.

--- hgwebbyproxy/server.py
+++ hgwebbyproxy/server.py
@@ -1,23 +1,20 @@
 import wsgi
 import sys
-import rocket
+import waitress
 
 
 if __name__ == '__main__':
-    try:
-        if (len(sys.argv) == 4):
-            # Create application
-            #
-            hgwebbyproxy_app = wsgi.HGWebByProxy()
+    if (len(sys.argv) == 4):
+        # Create application
+        #
+        hgwebbyproxy_app = wsgi.HGWebByProxy()
 
-            # Configure server
-            #
-            server_name = sys.argv[1]
-            server_host = sys.argv[2]
-            server_port = int(sys.argv[3])
-            server = rocket.Rocket((server_host, server_port), 'wsgi', {'wsgi_app': hgwebbyproxy_app}, 10, 25, 5, 600)
+        # Configure server
+        #
+        server_name = sys.argv[1]
+        server_host = sys.argv[2]
+        server_port = int(sys.argv[3])
 
-            server.start()
-
-    except KeyboardInterrupt:
-        server.stop()
+        # Launch server
+        #
+        waitress.serve(hgwebbyproxy_app, host=server_host, port=server_port)
